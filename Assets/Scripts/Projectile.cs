08a61a3741bfc050056fe430828824c1af845da2
Phuong Nguyen <haphuong311202@gmail.com>
2023-12-01 19:11:16 +0100

    Revert "Enemy + Projectile interaction"
    This reverts commit 90f25bb953b7adf1783f77ff02bfd6801c2b9550.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,20 +7,6 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField]
     float bulletSpeed;
-    public float bulletDamage;
-    private void FixedUpdate()
-    {
-        transform.position += Vector3.up * bulletSpeed * Time.deltaTime;
-        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-        if (Screen.width <= screenPosition.x || screenPosition.x <= 0 || Screen.height <= screenPosition.y || screenPosition.y <= 0)
-            Destroy(gameObject);
-    }
-        private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.GetComponent<Damageable>())
-        {
-            collision.gameObject.GetComponent<Damageable>().Damaged(bulletDamage);
-            Destroy(gameObject);
-        }
-    }
+    private void FixedUpdate() => transform.position += Vector3.up * bulletSpeed * Time.deltaTime;
+    private void OnBecameInvisible() => Destroy(gameObject);
 }
